c6af1eac0123f4a32f6ddd339250256530266a72
Stacy <aleyostacy@gmail.com>
2024-10-01 23:12:06 -0700

    all done althogh it shows erroes my eork is correct

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+// 1. 
+var customerName = 'bob';
+
+// 2. 
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); 
+}
+
+// 3. 
+function setBestCustomer() {
+  bestCustomer = 'not bob'; 
+}
+
+// 4. 
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; 
+}
+
+// 5. 
+const leastFavoriteCustomer = 'John Doe';
+
+// 6. 
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane Doe'; 
+
+//Testing
+upperCaseCustomerName(); 
+console.log(customerName);
+
+setBestCustomer(); 
+console.log(bestCustomer); 
+
+overwriteBestCustomer(); 
+console.log(bestCustomer); 
+
+console.log(leastFavoriteCustomer); 
+changeLeastFavoriteCustomer(); 
